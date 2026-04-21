3f0de0c7ab23af7589c45b416e63366e03a86c8f
Ingrid Connors <ing2135794@maricopa.edu>
2026-04-21 14:59:39 -0700

    choosing word and assigning variables to begin game

--- guess_the_word.py
+++ guess_the_word.py
@@ -6,3 +6,12 @@ import random
 
 # creating list of words that will be selected from for game
 word_list = ["apple", "calendar", "bookmark", "computer", "pencil", "spatula", "bandaid", "adapter"]
+
+word = random.choice(word_list) # selects random word from word_list
+guessed_letters = [] # creates list to store wrong guesses
+num_guesses = 10 # sets number of tries before game is over
+
+# displays _ for every letter in word
+display = ["_"] * len(word)
+
+print("Get ready to play Hangman!")
