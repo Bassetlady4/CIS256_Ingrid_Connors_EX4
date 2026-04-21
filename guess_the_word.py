897dacfc7f0fc5fc1402ab5c69ef46816ba947c9
Ingrid Connors <ing2135794@maricopa.edu>
2026-04-21 14:58:08 -0700

    creating the list of possible words

---
+++ guess_the_word.py
@@ -0,0 +1,8 @@
+# Ingrid Connors
+# CIS256 Spring 2026
+# Exercise Assignment 4
+
+import random
+
+# creating list of words that will be selected from for game
+word_list = ["apple", "calendar", "bookmark", "computer", "pencil", "spatula", "bandaid", "adapter"]
